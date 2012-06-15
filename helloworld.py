e3e672b7e8114170c53c44df4912f06a1ce7dd63
hedgehogtom <twionzek@gmail.com>
2012-06-15 10:12:20 -0700

    Reverting to Python 27 Google App Engine Hello World demo. This is version 1 of the file.

--- helloworld.py
+++ helloworld.py
@@ -3,10 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
-        self.response.out.write('Hi Tom this is Avner!')
-        self.response.out.write('This is Tom answering back!')
-        self.response.out.write('Avner fixed conflict')        
+        self.response.out.write('Hello, webapp World!')      
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug = True)
