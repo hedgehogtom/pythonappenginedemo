035a271088edbc135945eb1d9ef541b382f70686
hedgehogtom <twionzek@gmail.com>
2012-06-15 10:30:24 -0700

    V2: Hello, your_name

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,19 @@
-# V1: Google App Engine for Python 2.7
-#     1. Hello, World demo
+# V2: Google App Engine for Python 2.7
+#     2. Hello, your_name
 
 import webapp2
 
+from google.appengine.api import users
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')      
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug = True)
+                              debug=True)
