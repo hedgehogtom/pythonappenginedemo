8936fde5dfc4046e205f86704b23ce6ede8c289e
Avner May <avnermay@gmail.com>
2012-06-15 09:31:19 -0700

    Avner's first commit

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content Type'] = 'text/plain'
         self.response.out.write('Hello, webapp World!')
+        self.response.out.write('Hi Tom this is Avner!')
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug = True)
